b6ae8d1ed539145129e50a9da7bba25d90295ba8
Kehinde-Agboola <65411917+Kehinde-Agboola@users.noreply.github.com>
2022-11-09 17:54:18 +0100

    Navigation

---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <header>
+        <h1>Hello Home</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/component/data.js
@@ -0,0 +1,13 @@
+import React from "React";
+
+const Nav = [
+  {
+    title: "Become a supplier",
+  },
+  {
+    title: "Join Us",
+  },
+  {
+    title: "Login",
+  },
+];
